6d65e1b9e7971b6a95e199d2e093de5c905c7912
josecastillolema <josecastillolema@gmail.com>
2018-01-06 21:34:54 -0200

    Indentation problem corrected in MyTopo class

--- mini-nfv.py
+++ mini-nfv.py
@@ -38,14 +38,14 @@ class MyTopo(Topo):
     def __init__(self, n=2, **opts):
         Topo.__init__(self, **opts)
 
-	# Add hosts and switches
-    host1 = self.addHost('h1')
-    s1 = self.addSwitch('s1', dpid='00:00:00:00:00:01')
+        # Add hosts and switches
+        host1 = self.addHost('h1')
+        switch1 = self.addSwitch('s1', dpid='00:00:00:00:00:01')
 
-	# links Add
-    self.addLink(s1, host1, 1, 1)
-    # self.addLink(s2, s3, 2, 3, bw=10, delay='200ms', jitter='2ms', loss=10,
-    #              max_queue_size=1000, use_htb=True)
+        # links Add
+        self.addLink(switch1, host1, 1, 1)
+        # self.addLink(s2, s3, 2, 3, bw=10, delay='200ms', jitter='2ms', loss=10,
+        #              max_queue_size=1000, use_htb=True)
 
 def inicializa_red():
     "Create network and run simple performance test"
