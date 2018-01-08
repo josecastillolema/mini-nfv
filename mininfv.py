c8a8c489882f1e6a84ac7b906dd4a3eb054fa7af
josecastillolema <josecastillolema@gmail.com>
2018-01-08 15:21:02 -0200

    Changes

--- mininfv.py
+++ mininfv.py
@@ -33,7 +33,7 @@ def parse_vnffgd(path):
 
 class MyTopo(Topo):
     "Creates the mininet topology"
-    def __init__(self, k=2, **opts):
+    def __init__(self, **opts):
         Topo.__init__(self, **opts)
         # Add hosts and switches
         hosts = []
@@ -41,7 +41,7 @@ class MyTopo(Topo):
         while VNFD.has_key('VDU%s' % i):
             num_cpus = 1
             try:
-                num_cpus = VNFD['VDU%s' % i]['capabilities']['nfv_compute2']['properties']['num_cpus']
+                num_cpus = VNFD['VDU%s'%i]['capabilities']['nfv_compute2']['properties']['num_cpus']
                 if num_cpus >= 16:
                     num_cpus = 16
             except KeyError:
@@ -77,6 +77,7 @@ class MyTopo(Topo):
         #              max_queue_size=1000, use_htb=True)
 
 def configure_network(net):
+    "Configures the networks."
     host1 = net.getNodeByName('h1')
     i = 1
     while VNFD.has_key('VL%s' % i):
@@ -89,15 +90,15 @@ def configure_network(net):
         else:
             host1.setIP('10.0.%s.1/24', intf='h1-eth%s' % (i-1))
         i += 1
-    #host1.setIP('10.0.1.10', intf='h1-eth0')
-    #host1.setMAC('00:00:00:00:00:10', intf='h1-eth0')
-    #host1.setIP('10.0.5.11', intf='h1-eth1')
-    #host1.setIP('10.0.6.11', intf='h1-eth2')
-    #host1.setMAC('00:00:00:00:00:11', intf='h1-eth1')
+    # host1.setIP('10.0.1.10', intf='h1-eth0')
+    # host1.setMAC('00:00:00:00:00:10', intf='h1-eth0')
+    # host1.setIP('10.0.5.11', intf='h1-eth1')
+    # host1.setIP('10.0.6.11', intf='h1-eth2')
+    # host1.setMAC('00:00:00:00:00:11', intf='h1-eth1')
 
 def inicializa_red():
     "Create network and run simple performance test"
-    topo = MyTopo(k=4)
+    topo = MyTopo()
     #net = Mininet(topo=topo, link=TCLink, controller=RemoteController)
     #net = Mininet(topo=topo, link=TCLink)
     net = Mininet(topo=topo, link=TCLink, controller=OVSController)
