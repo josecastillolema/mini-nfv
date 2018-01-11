7a74a724f814f3b7122857e73fea5aaf48fa902b
josecastillolema <josecastillolema@gmail.com>
2018-01-11 17:58:05 -0200

    Corrected mask problem with add_host

--- mininfv.py
+++ mininfv.py
@@ -18,14 +18,16 @@ from mininet.node import RemoteController
 from mininet.cli import CLI, output
 
 MAX_VDUS = 100
-VNFDS = {}
+VNFD = {}
 VNFS = []
+VNFFGS = []
+VNFFGD = {}
 HOSTS = []
 SWITCH = {}
 INC = 10
 
-def parse_vnfd(path):
-    "Parses the yaml file corresponding to the vnfd"
+def parse_tosca(path):
+    "Parses the yaml file corresponding to the TOSCA vnfd ou vnnffgg template."
     try:
         yaml_file = open(path, 'r')
     except IOError:
@@ -33,11 +35,7 @@ def parse_vnfd(path):
         return None
     content = yaml_file.read()
     parsed_file = yaml.load(content)
-    return parsed_file['topology_template']['node_templates']
-
-def parse_vnffgd(path):
-    "Parses the yaml file corresponding to the vnffgd"
-    return path
+    return parsed_file
 
 class MyTopo(Topo):
     "Creates the mininet topology"
@@ -47,34 +45,35 @@ class MyTopo(Topo):
 def configure_network(net, vnfd, host):
     "Configures the networks."
     i = 1
-    switchs= []
-    while vnfd.has_key('VL%s' % i):
-        if vnfd['CP%s' % i]['properties'].has_key('ip_address'):
-            ip_address = vnfd['CP%s' % i]['properties']['ip_address']
+    switchs = []
+    topo = vnfd['topology_template']['node_templates']
+    while topo.has_key('VL%s' % i):
+        if topo['CP%s' % i]['properties'].has_key('ip_address'):
+            ip_address = topo['CP%s' % i]['properties']['ip_address']
             switch_name = 's' + ip_address
             switchs.append(switch_name)
             if not SWITCH.has_key(switch_name):
                 SWITCH[switch_name] = net.addSwitch(switch_name[:10])
         else:
-            if vnfd['VL%s' % i]['properties']['network_name'] == 'net_mgmt':
+            if topo['VL%s' % i]['properties']['network_name'] == 'net_mgmt':
                 switch_name = 's' + '192.168.120.0'
                 switchs.append(switch_name[:10])
                 if not SWITCH.has_key(switch_name):
                     SWITCH[switch_name] = net.addSwitch(switch_name[:10])
-            elif vnfd['VL%s' % i]['properties']['network_name'] == 'net0':
+            elif topo['VL%s' % i]['properties']['network_name'] == 'net0':
                 switch_name = 's' + '10.10.0.0'
                 switchs.append(switch_name[:10])
                 if not SWITCH.has_key(switch_name):
                     SWITCH[switch_name] = net.addSwitch(switch_name[:10])
-            elif vnfd['VL%s' % i]['properties']['network_name'] == 'net1':
+            elif topo['VL%s' % i]['properties']['network_name'] == 'net1':
                 switch_name = 's' + '10.10.1.0'
                 switchs.append(switch_name[:10])
                 if not SWITCH.has_key(switch_name):
                     SWITCH[switch_name] = net.addSwitch(switch_name[:10])
-            elif vnfd['VL%s' % i]['properties'].has_key('cidr'):
-                cidr = netaddr.IPNetwork(vnfd['VL%s' % i]['properties']['cidr'])
-                if vnfd['VL%s' % i]['properties'].has_key('start_ip'):
-                    start_ip = netaddr.IPNetwork(vnfd['VL%s' % i]['properties']['start_ip'])
+            elif topo['VL%s' % i]['properties'].has_key('cidr'):
+                cidr = netaddr.IPNetwork(topo['VL%s' % i]['properties']['cidr'])
+                if topo['node_templates']['VL%s' % i]['properties'].has_key('start_ip'):
+                    start_ip = netaddr.IPNetwork(topo['VL%s' % i]['properties']['start_ip'])
                     switch_name = 's%s' % start_ip.network
                     switchs.append(switch_name[:10])
                     if not SWITCH.has_key(switch_name):
@@ -95,24 +94,25 @@ def configure_host(net, vnfd, host):
     host1 = net.getNodeByName(host)
     i = 1
     global INC
-    while vnfd.has_key('VL%s' % i):
-        if vnfd['CP%s' % i]['properties'].has_key('ip_address'):
-            ip_address = vnfd['CP%s' % i]['properties']['ip_address']
+    topo = vnfd['topology_template']['node_templates']
+    while topo.has_key('VL%s' % i):
+        if topo['CP%s' % i]['properties'].has_key('ip_address'):
+            ip_address = topo['CP%s' % i]['properties']['ip_address']
             host1.setIP(ip_address, intf=host+'-eth%s' % (i-1))
         else:
-            if vnfd['VL%s' % i]['properties']['network_name'] == 'net_mgmt':
+            if topo['VL%s' % i]['properties']['network_name'] == 'net_mgmt':
                 host1.setIP('192.168.120.%s' % INC + '/24', intf=host+'-eth%s' % (i-1))
                 INC += 1
-            elif vnfd['VL%s' % i]['properties']['network_name'] == 'net0':
+            elif topo['VL%s' % i]['properties']['network_name'] == 'net0':
                 host1.setIP('10.10.0.%s' % INC+'/24', intf=host+'-eth%s' % (i-1))
                 INC += 1
-            elif vnfd['VL%s' % i]['properties']['network_name'] == 'net1':
+            elif topo['VL%s' % i]['properties']['network_name'] == 'net1':
                 host1.setIP('10.10.1.%s'%INC+'/24', intf=host+'-eth%s' % (i-1))
                 INC += 1
-            elif vnfd['VL%s' % i]['properties'].has_key('cidr'):
-                cidr = netaddr.IPNetwork(vnfd['VL%s' % i]['properties']['cidr'])
-                if vnfd['VL%s' % i]['properties'].has_key('start_ip'):
-                    start_ip = vnfd['VL%s' % i]['properties']['start_ip']
+            elif topo['VL%s' % i]['properties'].has_key('cidr'):
+                cidr = netaddr.IPNetwork(topo['VL%s' % i]['properties']['cidr'])
+                if topo['VL%s' % i]['properties'].has_key('start_ip'):
+                    start_ip = topo['VL%s' % i]['properties']['start_ip']
                     host1.setIP(start_ip+'/%s' % cidr.prefixlen, intf=host+'-eth%s' % (i-1))
                 else:
                     host1.setIP(str(cidr.ip+INC)+'/%s' % cidr.prefixlen, intf=host+'-eth%s' % (i-1))
@@ -120,8 +120,8 @@ def configure_host(net, vnfd, host):
             else:
                 host1.setIP('10.0.%s.%s/24' %(i, INC), intf=host+'-eth%s' % (i-1))
                 INC += 1
-        if vnfd['CP%s' % i]['properties'].has_key('mac_address'):
-            mac_address = vnfd['CP%s' % i]['properties']['mac_address']
+        if topo['CP%s' % i]['properties'].has_key('mac_address'):
+            mac_address = topo['CP%s' % i]['properties']['mac_address']
             host1.setMAC(mac_address, intf=host+'-eth%s' % (i-1))
         i += 1
 
@@ -130,14 +130,12 @@ def configure_host2(net, ips, host):
     host1 = net.getNodeByName(host)
     for i in range(len(ips)):
         ip_address = netaddr.IPNetwork(ips[i])
-        print ip_address.ip
-        host1.setIP('%s' % ip_address.ip, intf=host+'-eth%s' % (i))
+        host1.setIP('%s/%s' % (ip_address.ip, ip_address.prefixlen), intf=host+'-eth%s' % i)
 
 def add_host(self, line):
     "Adds a host to the mininet topology."
     net = self.mn
     if len(line.split()) < 1:
-        output('Wrong number or arguments\n')
         output('Use: add_host <HOST-NAME> [<IP1/masc> <IP2/masc> ...]\n')
         return None
     host_name = line.split()[0]
@@ -168,7 +166,7 @@ def add_host(self, line):
 def cloud_init(net, vnfd, host_name):
     "Configures the networks."
     host = net.getNodeByName(host_name)
-    cloudinit = vnfd['VDU1']['properties']['user_data']
+    cloudinit = vnfd['topology_template']['node_templates']['VDU1']['properties']['user_data']
     output('*** Initializing VDU ' + host_name + ' ...\n')
     host.cmdPrint(cloudinit)
 
@@ -176,26 +174,27 @@ def cloud_init(net, vnfd, host_name):
 
 def vnfd_create(self, line):
     "Creates vnfd from template."
-    net = self.mn
     if len(line.split()) != 3 or line.split()[0] != '--vnfd-file':
         output('Use: vnfd_create --vnfd-file <yaml file path> <VNFD-NAME>\n')
         return None
     file_path = line.split()[1]
-    vnfd = parse_vnfd(file_path)
+    vnfd = parse_tosca(file_path)
     if vnfd:
         vnfd_name = line.split()[2]
-        if not VNFDS.has_key(vnfd_name):
-            VNFDS[vnfd_name] = vnfd
+        if not VNFD.has_key(vnfd_name):
+            VNFD[vnfd_name] = vnfd
         else:
             output('<VNFD-NAME> already in use\n')
     return None
 
 def vnfd_list(self, line):
-    "Lists all vnfds uploaded."
+    "Lists all VNFD uploaded."
     if line:
         output('Use: vnfd_list\n')
         return None
-    output('%s' % VNFDS.keys() + '\n')
+    for i in VNFD:
+        output('%s: %s\n' % (i, VNFD[i]['description']))
+    #output('%s' % VNFD.keys() + '\n')
     return None
 
 def vnfd_delete(self, line):
@@ -204,8 +203,8 @@ def vnfd_delete(self, line):
         output('Use: vnfd_delete <VNFD-NAME>\n')
         return None
     vnfd_name = line.split()[0]
-    if VNFDS.has_key(vnfd_name):
-        del VNFDS[vnfd_name]
+    if VNFD.has_key(vnfd_name):
+        del VNFD[vnfd_name]
     else:
         output('<VNFD-NAME> does not exist\n')
     return None
@@ -217,7 +216,7 @@ def vnfd_template_show(self, line):
         output('Use: vnfd_template_show <VNFD-NAME>\n')
         return None
     vnfd_name = line.split()[0]
-    output(('%s' % VNFDS[vnfd_name])+'\n')
+    output(('%s' % VNFD[vnfd_name])+'\n')
     return None
 
 # VNF
@@ -232,10 +231,10 @@ def vnf_create(self, line):
         return None
     if line.split()[0] in ['--vnfd-file', '--vnfd-template']:
         file_path = line.split()[1]
-        vnfd = parse_vnfd(file_path)
+        vnfd = parse_tosca(file_path)
     else:  # --vnfd-name
         vnfd_name = line.split()[1]
-        vnfd = VNFDS[vnfd_name]
+        vnfd = VNFD[vnfd_name]
     if vnfd:
         vnf_name = line.split()[2]
         if vnf_name in VNFS:
@@ -245,7 +244,7 @@ def vnf_create(self, line):
         net.addHost(vnf_name)
         configure_network(net, vnfd, vnf_name)
         configure_host(net, vnfd, vnf_name)
-        if vnfd['VDU1']['properties'].has_key('user_data'):
+        if vnfd['topology_template']['node_templates']['VDU1']['properties'].has_key('user_data'):
             cloud_init(net, vnfd, vnf_name)
         return None
     return None
@@ -269,6 +268,38 @@ def vnf_delete(self, line):
         output('<VNF-NAME> does not exist\n')
     return None
 
+# VNFFG
+
+def vnffg_create(self, line):
+    "Creates vnffgd from from template."
+    if len(line.split()) != 7 or line.split()[0] not in ['--vnfd-name', '--vnfd-template'] or line.split()[3] != '--vnf-mapping' or line.split()[5] != '--symmetrical':
+        output('''Use: vnffg-create --vnffgd-name <vnffgd-name> --vnf-mapping <vnf-mapping>
+                                    --symmetrical <boolean> <vnffg-name>\n''')
+        output('''     vnffg-create --vnffgd-template <vnffgd-template> --vnf-mapping <vnf-mapping>
+                                    --symmetrical <boolean> <vnffg-name>\n''')
+        return None
+    if line.split()[0] == ['--vnfd-template']:
+        file_path = line.split()[1]
+        vnffg = parse_tosca(file_path)
+    else:  # --vnffg-name
+        vnffg_name = line.split()[1]
+        vnffg = VNFFGD[vnffg_name]
+    if vnffg:
+        vnffg_name = line.split()[6]
+        #if vnffg_name in VNFS:
+        #    output('<VNF-NAME> already in use\n')
+        #    return None
+        #VNFS.append(vnf_name)
+        #net.addHost(vnf_name)
+        #configure_network(net, vnfd, vnf_name)
+        #configure_host(net, vnfd, vnf_name)
+        return None
+    return None
+
+def vnffg_list(self, line):
+    "Lists all vnfs created."
+    output('%s' % VNFFGS + '\n')
+
 if __name__ == '__main__':
     setLogLevel('info')
     if len(sys.argv) > 2:
@@ -276,15 +307,15 @@ if __name__ == '__main__':
     elif len(sys.argv) == 2 and sys.argv[1] != '--standalone':
         sys.exit('Uso: ./mininfv [--standalone]')
     elif len(sys.argv) == 2 and sys.argv[1] == '--standalone':
-        standalone = True
+        STANDALONE = True
     else:
-        standalone = False
+        STANDALONE = False
 
     TOPO = MyTopo()
     # NET = Mininet(topo=topo, link=TCLink, controller=RemoteController)
     # NET = Mininet(topo=topo, link=TCLink)
     # NET = Mininet(topo=TOPO, link=TCLink, controller=OVSController)
-    if standalone:
+    if STANDALONE:
         NET = Mininet(topo=TOPO, link=TCLink, controller=OVSController)
     else:
         NET = Mininet(topo=TOPO, link=TCLink, controller=RemoteController)
